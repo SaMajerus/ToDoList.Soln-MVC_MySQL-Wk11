fe480f3eced54565c6403af1254804cdc4adca8a
Samuel Majerus <ladolego@gmail.com>
2022-10-09 21:47:32 -0700

    Updates 'Delete()' functionality using the EF Core concept called ''cascade delete''. [End of code-along work for Lsn 8 (see link).] https://www.learnhowtoprogram.com/c-and-net/many-to-many-relationships/many-to-many-delete-functionality

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -86,19 +86,19 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
-    // public ActionResult Delete(int id)
-    // {
-    //     var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //     return View(thisItem);
-    // }
+    public ActionResult Delete(int id)
+    {
+        var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+        return View(thisItem);
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //     var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //     _db.Items.Remove(thisItem);
-    //     _db.SaveChanges();
-    //     return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult DeleteCategory(int joinId)
+    {
+      var joinEntry = _db.CategoryItem.FirstOrDefault(entry => entry.CategoryItemId == joinId);
+      _db.CategoryItem.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
